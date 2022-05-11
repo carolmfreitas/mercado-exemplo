820e2366f6b8df4844aa29ec89473dc1bf8bfac7
120110894 CAROLINA MARTINS FREITAS <carolinamartinsfreitas@gmail.com>
2022-05-11 17:30:50 -0300

    adicionando estoque

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,3 +1,4 @@
+import exemplo.Estoque;
 import exemplo.Lote;
 import exemplo.Produto;
 
@@ -7,8 +8,10 @@ public class Sistema {
 		
 		Produto produto = new Produto("Leite", "Parmalat");
 		Lote lote = new Lote(produto, 10L);
+		Estoque estoque = new Estoque();
 		
 		System.out.println(produto);
 		System.out.println(lote);
 	}
 }
+
